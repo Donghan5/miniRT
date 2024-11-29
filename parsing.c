15e56956f7210779ba63c6c46d4896ae587b8e9b
Donghan Kim <donghank@made-f0Ar10s6.clusters.42paris.fr>
2024-11-29 13:25:28 +0100

    test in dell environment

--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: donghank <donghank@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 17:39:31 by pzinurov          #+#    #+#             */
-/*   Updated: 2024/11/29 00:09:34 by donghank         ###   ########.fr       */
+/*   Updated: 2024/11/29 13:12:41 by donghank         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -164,12 +164,12 @@ void	init_scene(char *path, t_scene *scene)
 	// sphere part
 	int	sp_idx;
 	sp_idx = 0;
-	scene->sphere = malloc(sizeof(t_sphere *) * scene->sphere_n);
+	scene->sphere = ft_calloc(scene->sphere_n, sizeof(t_sphere *));
 	if (!scene->sphere)
 		handle_error("Fail dynamic allocate t_sphere scope all");
 	while (sp_idx < scene->sphere_n)
 	{
-		scene->sphere[sp_idx] = malloc(sizeof(t_sphere));
+		scene->sphere[sp_idx] = ft_calloc(1, sizeof(t_sphere *));
 		if (!scene->sphere[sp_idx])
 			handle_error("Fail dynamic allocate t_sphere scope index");
 		scene->sphere[sp_idx]->color.r = 0;
@@ -185,12 +185,12 @@ void	init_scene(char *path, t_scene *scene)
 	//plane part
 	int	pl_idx;
 	pl_idx = 0;
-	scene->plane = malloc(sizeof(t_plane *) * scene->plane_n);
+	scene->plane = ft_calloc(scene->plane_n, sizeof(t_plane *));
 	if (!scene->plane)
 		handle_error("Fail dynamic allocate t_plane");
 	while (pl_idx < scene->plane_n)
 	{
-		scene->plane[pl_idx] = malloc(sizeof(t_plane));
+		scene->plane[pl_idx] = ft_calloc(1, sizeof(t_plane *));
 		if (!scene->plane[pl_idx])
 			handle_error("Fail dynamic allocate t_plane");
 		scene->plane[pl_idx]->color.r = 0;
@@ -206,12 +206,12 @@ void	init_scene(char *path, t_scene *scene)
 	// cylinder part
 	int	cy_idx;
 	cy_idx = 0;
-	scene->cylinder = malloc(sizeof(t_cylinder *));
+	scene->cylinder = ft_calloc(scene->cylinder_n, sizeof(t_cylinder *));
 	if (!scene->cylinder)
 		handle_error("Fail to dynamic allocate t_cylinder");
 	while (cy_idx < scene->cylinder_n)
 	{
-		scene->cylinder[cy_idx] = malloc(sizeof(t_cylinder));
+		scene->cylinder[cy_idx] = ft_calloc(1, sizeof(t_cylinder));
 		if (!scene->cylinder[cy_idx])
 			handle_error("Fail dynamic allocate t_cylinder");
 		scene->cylinder[cy_idx]->axis_vector.y = 0.0;
@@ -268,28 +268,25 @@ int	get_type(char *map_info)
 		info_map: information of the map which read by gnl
 		sep_info: separate information of info_map
 		rgb_infos: RGB information
-	@return
-		1: success
-		0: fail
+
 */
-int	stock_ambient(t_scene *scene, char *info_map)
+void	stock_ambient(t_scene *scene, char *info_map)
 {
 	char	**sep_info;
 	char	**rgb_infos;
 
 	sep_info = ft_split(info_map, ' ');
 	if (sep_info == NULL)
-		return (printf("%s\n", PARSE_ERR), 0);
+		handle_error(PARSE_ERR);
 	scene->ambient.a_ratio = ft_atod(sep_info[1]);
 	rgb_infos = ft_split((char *)sep_info[2], ',');
 	if (rgb_infos == NULL)
-		return (printf("%s\n", PARSE_RGB_ERR), 0);
+		handle_error(PARSE_RGB_ERR);
 	scene->ambient.color.r = ft_atoi(rgb_infos[0]);
 	scene->ambient.color.g = ft_atoi(rgb_infos[1]);
 	scene->ambient.color.b = ft_atoi(rgb_infos[2]);
 	free_doub_array(rgb_infos);
 	free_doub_array(sep_info);
-	return (1);
 }
 
 /*
@@ -300,11 +297,9 @@ int	stock_ambient(t_scene *scene, char *info_map)
 		sep_info: same with stock_ambient
 		coord_info: coordination information of camera
 		orient_info: orientation vector information of camera
-	@return
-		1: success
-		0: fail
+
 */
-int	stock_cam(t_scene *scene, char *info_map)
+void	stock_cam(t_scene *scene, char *info_map)
 {
 	char	**sep_info;
 	char	**coord_info;
@@ -312,24 +307,23 @@ int	stock_cam(t_scene *scene, char *info_map)
 
 	sep_info = ft_split(info_map, ' ');
 	if (sep_info == NULL)
-		return (printf("%s\n", PARSE_ERR), 0);
+		handle_error(PARSE_ERR);
 	coord_info = ft_split((char *)sep_info[1], ',');
 	if (coord_info == NULL)
-		return (printf("%s\n", COOR_ERR), 0);
+		handle_error(COOR_ERR);
 	scene->camera.coordinates.x = ft_atod(coord_info[0]);
 	scene->camera.coordinates.y = ft_atoi(coord_info[1]);
 	scene->camera.coordinates.z = ft_atoi(coord_info[2]);
 	free_doub_array(coord_info);
 	orient_info = ft_split((char *)sep_info[2], ',');
 	if (orient_info == NULL)
-		return (printf("%s\n", ORI_ERR), 0);
+		handle_error(ORI_ERR);
 	scene->camera.orientation.x = ft_atod(orient_info[0]);
 	scene->camera.orientation.y = ft_atod(orient_info[1]);
 	scene->camera.orientation.z = ft_atod(orient_info[2]);
 	scene->camera.c_view = ft_atod(sep_info[3]);
 	free_doub_array(orient_info);
 	free_doub_array(sep_info);
-	return (1);
 }
 
 /*
@@ -340,11 +334,9 @@ int	stock_cam(t_scene *scene, char *info_map)
 		sep_info: same with function stock_ambient
 		coord_info: coordinate information of light
 		rgb_infos: RGB information of light
-	@return
-		1: success
-		0: fail
+
 */
-int	stock_light(t_scene *scene, char *info_map)
+void	stock_light(t_scene *scene, char *info_map)
 {
 	char	**sep_info;
 	char	**coord_info;
@@ -352,10 +344,10 @@ int	stock_light(t_scene *scene, char *info_map)
 
 	sep_info = ft_split(info_map, ' ');
 	if (sep_info == NULL)
-		return (printf("%s\n", PARSE_ERR), 0);
+		handle_error(PARSE_ERR);
 	coord_info = ft_split((char *)sep_info[1], ',');
 	if (coord_info == NULL)
-		return (printf("%s\n", COOR_ERR), 0);
+		handle_error(COOR_ERR);
 	scene->light.coordinates.x = ft_atod(coord_info[0]);
 	scene->light.coordinates.y = ft_atod(coord_info[1]);
 	scene->light.coordinates.z = ft_atod(coord_info[2]);
@@ -363,13 +355,12 @@ int	stock_light(t_scene *scene, char *info_map)
 	scene->light.l_brightness = ft_atod(sep_info[2]);
 	rgb_infos = ft_split((char *)sep_info[3], ',');
 	if (rgb_infos == NULL)
-		return (printf("%s\n", PARSE_RGB_ERR), 0);
+		handle_error(PARSE_RGB_ERR);
 	scene->light.color.r = ft_atoi(rgb_infos[0]);
 	scene->light.color.g = ft_atoi(rgb_infos[1]);
 	scene->light.color.b = ft_atoi(rgb_infos[2]);
 	free_doub_array(rgb_infos);
 	free_doub_array(sep_info);
-	return (1);
 }
 
 /*
@@ -380,11 +371,9 @@ int	stock_light(t_scene *scene, char *info_map)
 		coord_info: coordination information of plane
 		orient_info: normal vector information of plane
 		rgb_infos: RGB information of plane
-	@return
-		1: success
-		0: fail
+
 */
-int	stock_plane(t_scene *scene, char *info_map)
+void	stock_plane(t_scene *scene, char *info_map)
 {
 	char	**sep_info;
 	char	**coord_info;
@@ -393,27 +382,29 @@ int	stock_plane(t_scene *scene, char *info_map)
 
 	sep_info = ft_split(info_map, ' ');
 	if (sep_info == NULL)
-		return (printf("%s\n", PARSE_ERR), 0);
+		handle_error(PARSE_ERR);
 	coord_info = ft_split(sep_info[1], ',');
+	if (coord_info == NULL)
+		handle_error(COOR_ERR);
 	(*scene->plane)->coordinates.x = ft_atod(coord_info[0]);
 	(*scene->plane)->coordinates.y = ft_atod(coord_info[1]);
 	(*scene->plane)->coordinates.z = ft_atod(coord_info[2]);
 	free_doub_array(coord_info);
 	orient_info = ft_split(sep_info[2], ',');
 	if (orient_info == NULL)
-		return (printf("%s\n", ORI_ERR), 0);
+		handle_error(ORI_ERR);
 	(*scene->plane)->normal_vector.x = ft_atod(orient_info[0]);
 	(*scene->plane)->normal_vector.y = ft_atod(orient_info[1]);
 	(*scene->plane)->normal_vector.z = ft_atod(orient_info[2]);
 	free_doub_array(orient_info);
 	rgb_infos = ft_split(sep_info[3], ',');
+	if (rgb_infos == NULL)
+		handle_error(PARSE_RGB_ERR);
 	(*scene->plane)->color.r = ft_atoi(rgb_infos[0]);
 	(*scene->plane)->color.g = ft_atoi(rgb_infos[1]);
 	(*scene->plane)->color.b = ft_atoi(rgb_infos[2]);
 	free_doub_array(rgb_infos);
 	free_doub_array(sep_info);
-	scene->plane_n++;
-	return (1);
 }
 
 /*
@@ -424,11 +415,8 @@ int	stock_plane(t_scene *scene, char *info_map)
 		sep_info: same with stock_ambient()
 		coord_info: coordination information of sphere
 		rgb_infos: RGB information of sphere
-	@return
-		1: success
-		0: fail
 */
-int	stock_sphere(t_scene *scene, char *info_map)
+void	stock_sphere(t_scene *scene, char *info_map)
 {
 	char	**sep_info;
 	char	**coord_info;
@@ -436,10 +424,10 @@ int	stock_sphere(t_scene *scene, char *info_map)
 
 	sep_info = ft_split(info_map, ' ');
 	if (sep_info == NULL)
-		return (printf("%s\n", PARSE_ERR), 0);
+		handle_error(PARSE_ERR);
 	coord_info = ft_split(sep_info[1], ',');
 	if (coord_info == NULL)
-		return (printf("%s\n", COOR_ERR), 0);
+		handle_error(COOR_ERR);
 	(*scene->sphere)->coordinates.x = ft_atod(coord_info[0]);
 	(*scene->sphere)->coordinates.y = ft_atod(coord_info[1]);
 	(*scene->sphere)->coordinates.z = ft_atod(coord_info[2]);
@@ -447,14 +435,12 @@ int	stock_sphere(t_scene *scene, char *info_map)
 	(*scene->sphere)->sp_diameter = ft_atod(sep_info[2]);
 	rgb_infos = ft_split((char *)sep_info[3], ',');
 	if (rgb_infos == NULL)
-		return (printf("%s\n", PARSE_RGB_ERR), 0);
+		handle_error(PARSE_RGB_ERR);
 	(*scene->sphere)->color.r = ft_atoi(rgb_infos[0]);
 	(*scene->sphere)->color.g = ft_atoi(rgb_infos[1]);
 	(*scene->sphere)->color.b = ft_atoi(rgb_infos[2]);
 	free_doub_array(rgb_infos);
 	free_doub_array(sep_info);
-	scene->sphere_n++;
-	return (1);
 }
 
 /*
@@ -466,11 +452,8 @@ int	stock_sphere(t_scene *scene, char *info_map)
 		coord_info: coordinate information of cylinder
 		orient_info: normal vector information of cylinder
 		rgb_infos: RGB information of cylinder
-	@return
-		1: success
-		0: fail
 */
-int	stock_cylinder(t_scene *scene, char *info_map)
+void	stock_cylinder(t_scene *scene, char *info_map)
 {
 	char	**sep_info;
 	char	**coord_info;
@@ -479,17 +462,17 @@ int	stock_cylinder(t_scene *scene, char *info_map)
 
 	sep_info = ft_split(info_map, ' ');
 	if (sep_info == NULL)
-		return (printf("%s\n", PARSE_ERR), 0);
+		handle_error(PARSE_ERR);
 	coord_info = ft_split(sep_info[1], ',');
 	if (coord_info == NULL)
-		return (printf("%s\n", COOR_ERR), 0);
+		handle_error(COOR_ERR);
 	(*scene->cylinder)->coordinates.x = ft_atod(coord_info[0]);
 	(*scene->cylinder)->coordinates.y = ft_atod(coord_info[1]);
 	(*scene->cylinder)->coordinates.z = ft_atod(coord_info[2]);
 	free_doub_array(coord_info);
 	orient_info = ft_split((char *)sep_info[2], ',');
 	if (orient_info == NULL)
-		return (printf("%s\n", ORI_ERR), 0);
+		handle_error(ORI_ERR);
 	(*scene->cylinder)->axis_vector.x = ft_atod(orient_info[0]);
 	(*scene->cylinder)->axis_vector.y = ft_atod(orient_info[1]);
 	(*scene->cylinder)->axis_vector.z = ft_atod(orient_info[2]);
@@ -498,42 +481,29 @@ int	stock_cylinder(t_scene *scene, char *info_map)
 	(*scene->cylinder)->cy_height = ft_atod(sep_info[4]);
 	rgb_infos = ft_split(sep_info[5], ',');
 	if (rgb_infos == NULL)
-		return (printf("%s\n", PARSE_RGB_ERR), 0);
+		handle_error(PARSE_RGB_ERR);
 	(*scene->cylinder)->color.r = ft_atoi(rgb_infos[0]);
 	(*scene->cylinder)->color.g = ft_atoi(rgb_infos[1]);
 	(*scene->cylinder)->color.b = ft_atoi(rgb_infos[2]);
 	free_doub_array(rgb_infos);
 	free_doub_array(sep_info);
-	scene->cylinder_n++;
-	return (1);
 }
 
 /*
+	part of ambient, camera and light
 	@param
 		type: type of the map_infos[1]
 		scene: structure to stock the infos
 		info_map: numeric information of the map
-		indices: variable to trace index of the objs
-	@return
-		1: success
-		0: fail
 */
-int	stock_infos(int type, t_scene *scene, char *info_map, int *indices)
+void	stock_infos(int type, t_scene *scene, char *info_map)
 {
 	if (type == 1)
-		return (stock_ambient(scene, info_map), 1);
+		stock_ambient(scene, info_map);
 	else if (type == 2)
-		return (stock_cam(scene, info_map), 1);
+		stock_cam(scene, info_map);
 	else if (type == 3)
-		return (stock_light(scene, info_map), 1);
-	else if (type == 4)
-		return (stock_plane(scene->plane[indices[0]++], info_map), 1);
-	else if (type == 5)
-		return (stock_sphere(scene->sphere[indices[1]++], info_map), 1);
-	else if (type == 6)
-		return (stock_cylinder(scene->sphere[indices[2]++], info_map), 1);
-	else
-		return (0);
+		stock_light(scene, info_map);
 }
 
 /*
@@ -555,7 +525,7 @@ void	parse_scene(char *path, t_scene *scene)
 	char	*map_line;
 	int		type;
 	int		fd;
-	int	indices[3] = {0, 0, 0}; // I'm not sure it is forbidden in norm
+	int		pl_idx = 0, sp_idx = 0, cy_idx = 0;
 
 	init_scene(path, scene);
 	fd = open(path, O_RDONLY);
@@ -577,11 +547,14 @@ void	parse_scene(char *path, t_scene *scene)
 			free(map_line);
 			handle_error("Invalid type number");
 		}
-		if (stock_infos(type, scene, map_line, indices) == 0)
-		{
-			free(map_line);
-			handle_error("Failure inside of parsing part");
-		}
+		if (type == 1 || type == 2 || type == 3)
+			stock_infos(type, scene, map_line);
+		else if (type == 4)
+			stock_plane(scene->plane[pl_idx++], map_line);
+		else if (type == 5)
+			stock_sphere(scene->sphere[sp_idx++], map_line);
+		else if (type == 6)
+			stock_cylinder(scene->cylinder[cy_idx++], map_line);
 		free(map_line);
 		map_line = get_next_line(fd);
 	}
